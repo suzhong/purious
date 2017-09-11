ef4ea387040b8cf9d3d673a04ac78624acef08f7
zuhong <1962250257@qq.com>
2017-09-11 16:12:34 +0800

    韦德教育seo官方网站mobile端的代码，保存到GitHub上

---
+++ style/header.js
@@ -0,0 +1,8 @@
+var mySwiper = new Swiper('.swiper-container',{
+  pagination : '.swiper-pagination',
+  paginationClickable :true,
+  loop : true,
+  loopAdditionalSlides : 0,
+  autoplay : 2000,
+  autoplayDisableOnInteraction : false,
+})
